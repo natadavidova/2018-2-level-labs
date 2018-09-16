9c6d2892bff728cbee0771213f381edabd9f4b20
Alexander Demidovskij <alexander.demidovskij@intel.com>
2018-09-16 10:24:24 +0300

    added the correct status for looping checks

---
+++ config/run-tests.sh
@@ -0,0 +1,12 @@
+echo "$LABS_TO_BUILD"
+WAS_FAILED=false
+for i in ${LABS_TO_BUILD[@]}; do
+	if ! python3 -m unittest discover -p *_test.py -s lab_${i};  then
+    	WAS_FAILED=true
+	fi
+done
+
+if [ "$WAS_FAILED" = true ]; then
+	echo tests failed
+	return 1
+fi
