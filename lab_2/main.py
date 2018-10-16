bf96e69d226ba6c9c466afb2a98c01360f2c6332
natadavidova <44080391+natadavidova@users.noreply.github.com>
2018-10-16 10:37:31 +0300

    Test change

--- lab_2/main.py
+++ lab_2/main.py
@@ -7,6 +7,8 @@ from lab_1.main import calculate_frequences
 LETTERS = 'abcdefghijklmnopqrstuvwxyz'
 REFERENCE_TEXT = ''
 
+print(REFERENCE_TEXT)
+
 if __name__ == '__main__':
     with open('very_big_reference_text.txt', 'r') as f:
         REFERENCE_TEXT = f.read()
