edca983ecbf11b2744da51c8f8c6f20a06023f64
Marat Fatekhov <marat.fatekhov@intel.com>
2018-12-12 23:41:23 +0300

    tests done

---
+++ lab_4/main.py
@@ -0,0 +1,39 @@
+import math
+
+REFERENCE_TEXTS = []
+if __name__ == '__main__':
+    texts = ['5_7.txt', '15_2.txt', '10547_3.txt', '12230_7.txt']
+    for text in texts:
+        with open(text, 'r') as f:
+            REFERENCE_TEXTS.append(f.read())
+
+
+def clean_tokenize_corpus(texts: list) -> list:
+    pass
+
+
+class TfIdfCalculator:
+    def __init__(self, corpus):
+        pass
+
+    def calculate_tf(self):
+        pass
+
+    def calculate_idf(self):
+        pass
+
+    def calculate(self):
+        pass
+
+    def report_on(self, word, document_index):
+        pass
+
+
+# scenario to check your work
+test_texts = clean_tokenize_corpus(REFERENCE_TEXTS)
+tf_idf = TfIdfCalculator(test_texts)
+tf_idf.calculate_tf()
+tf_idf.calculate_idf()
+tf_idf.calculate()
+print(tf_idf.report_on('good', 0))
+print(tf_idf.report_on('and', 1))
